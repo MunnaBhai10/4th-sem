ceedf825844f778ed2475b09a2557e8dbe22fe8d
[prabesh kharel] <[prabeshkharel168@gmail.com]>
2024-08-04 12:57:26 +0545

    conditional statement

---
+++ Scripting language/conditional_statement/cs.js
@@ -0,0 +1,59 @@
+// - if
+// -if else
+// -nested if else
+// -switch
+
+// #syntax
+// if(condition){
+//     statement
+// }
+
+
+// let student = "prabesh"
+// if(student=="prabesh"){
+//     console.log(student+" Kharel")
+// }else{
+//     console.log("who is he?");
+    
+// }
+
+// nested if statement 
+
+
+
+// let isStudent = 10
+// if(isStudent=="10"){
+
+    
+//     console.log(isStudent+" Kharel")
+    
+// }else{
+//     console.log("who is he?");
+    
+// }
+
+// Swtich 
+// let Day = "sunday"
+//  switch(Day){
+//     case "sunday":
+//         console.log(Day +" is Funday")
+//         break;
+//         case "monday": 
+//         console.log(Day+" is barta day");
+//         break;
+//         case "tuesday":
+//             console.log(Day +" is fuesday");
+//             break;
+//             default: 
+//             console.log("enter valid day");
+            
+            
+        
+//  }
+
+
+//for loop
+for(let i =1;i<=100;i++){
+    console.log(i);
+    
+}
